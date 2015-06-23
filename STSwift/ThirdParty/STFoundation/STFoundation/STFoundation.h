e4c86030b6cd5b82bd10c532c792f03f01cfd772
tusu <striving_boy@163.com>
2015-06-23 23:58:43 +0800

    add foundation project
    Signed-off-by: tusu <striving_boy@163.com>

---
+++ STSwift/ThirdParty/STFoundation/STFoundation/STFoundation.h
@@ -0,0 +1,19 @@
+//
+//  STFoundation.h
+//  STFoundation
+//
+//  Created by tusu on 15/6/23.
+//  Copyright (c) 2015年 strivingboy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for STFoundation.
+FOUNDATION_EXPORT double STFoundationVersionNumber;
+
+//! Project version string for STFoundation.
+FOUNDATION_EXPORT const unsigned char STFoundationVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <STFoundation/PublicHeader.h>
+
+
